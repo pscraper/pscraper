2f2e09cee87a636c907077fdc317f14981a80882
rivercity310 <h970126@gmail.com>
2024-02-15 20:25:48 +0900

    feat: 프로젝트 배포를 위한 패키지화

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = "pscraper",
-    maintainer = "seungsu hwang",
-    version = "0.1.0",
+    name = "pscraper",      # pip이 참조하는 이름
+    author = "seungsu hwang",
+    author_email = "sshwang.intern@ahnlab.com",
+    version = "0.1.1",
+    entry_point = {
+        "console-scripts": ["pscraper=src.pscraper:run"]
+    },
     packages = find_packages(where = "src.*")
 )
