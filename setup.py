898a4da73b35a7362a3e06e5e9cf6cf5eb38cfc7
rivercity310 <h970126@gmail.com>
2024-02-15 20:49:03 +0900

    feat: pypi 배포

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import find_packages, setup
 
+
+# pypi 배포 
+# python -m pip install pscraper==0.1.1
+# https://devocean.sk.com/blog/techBoardDetail.do?ID=163566
+
 setup(
     name = "pscraper",      # pip이 참조하는 이름
     author = "seungsu hwang",
